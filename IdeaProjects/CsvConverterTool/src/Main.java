72d08b415820bdb9d5c2901855c07786f3e5232c
mikhail <m.donskoi9@gmail.com>
2016-07-01 23:14:01 +0300

    FileWriter added

--- IdeaProjects/CsvConverterTool/src/Main.java
+++ IdeaProjects/CsvConverterTool/src/Main.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -6,10 +6,21 @@ import java.nio.file.Paths;
 
 public class Main {
 
+    private static int n = 0;
+    private static String line;
+
     public static void main(String[] args) {
-        
-        
+
+
         String FILE_NAME = "/Users/mikhail/Desktop/Лист1-Table 1.csv";
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(FILE_NAME));
+
+            n = (int) br.lines().count();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
         long STARTTIME = System.currentTimeMillis();
 
@@ -19,12 +30,56 @@ public class Main {
             e.printStackTrace();
         }
 
-        long ENDTIME = System.currentTimeMillis();
-        
-        System.out.println();
-        System.out.print("Time spent: ");
-        System.out.println(ENDTIME-STARTTIME + " mlsec");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(FILE_NAME));
+            while (br.lines().count() != 0) {
+                line = br.readLine();
+                System.out.println(br.lines() + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            }
 
-        // TODO: 6/30/16 make CSV writer 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        ///////////////
+
+        try {
+
+            String content = line;
+
+            File file = new File("/Users/mikhail/Desktop/Лист1-Table 11111.csv");
+
+            // if file doesnt exists, then create it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            bw.write(content);
+            bw.close();
+
+            System.out.println(" Write Done");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ////////////
+
+        long ENDTIME = System.currentTimeMillis();
+
+
+        System.out.println();
+        System.out.println(n + " lines");
+        System.out.print("Time spent: ");
+        System.out.println(ENDTIME - STARTTIME + " mlsec");
+
+
+        // TODO: 6/30/16 make CSV writer
     }
 }
